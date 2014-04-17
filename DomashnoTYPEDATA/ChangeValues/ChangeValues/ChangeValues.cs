e82022c9b67615f41f5904928279fe4370738d09
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 15:03:13 +0200

    C# fundamentals - homework

---
+++ DomashnoTYPEDATA/ChangeValues/ChangeValues/ChangeValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+
+    class Values
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = 10;
+            int c;
+            c = b;
+            b = a;
+            a = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+
