1fa4b386c914a42a7c85dfb9ef44be5ce7b1d149
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 15:43:11 +0200

    Using Classes and Objects

---
+++ 05.HomeworkUseingClasses/01.ReadsYear/ReadsYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class ReadsYear
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the year:");
+            int yearNum = int.Parse(Console.ReadLine());
+            DateTime year = new DateTime(yearNum);
+
+            Console.WriteLine("Is {0} leap year: {1}", yearNum, DateTime.IsLeapYear(yearNum));
+           
+           
+        }
+    }
+
