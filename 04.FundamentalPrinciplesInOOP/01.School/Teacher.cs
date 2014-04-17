a7ccb34df3ec0ba4284e7a8bc948d86c768b94db
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 15:33:50 +0200

    OOP in C#

---
+++ 04.FundamentalPrinciplesInOOP/01.School/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.School
+{
+    class Teacher : Person, ICommentable
+    {
+        public List<Discipline> Discplines { get; set; }
+
+        public Teacher(string name)
+        {
+            this.Discplines = new List<Discipline>();
+            this.Name = name;
+        }
+        public List<string> Comments { get; set; }
+
+        public void AddComment(string comment)
+        {
+            Comments.Add(comment);
+        }
+      
+    }
+}
