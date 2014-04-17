446a26198467cbb97d8dc434df81b4acebcddcd5
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 14:57:01 +0200

    e

---
+++ 03Homework.Methods/UnitTestNumberInArray/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestNumberInArray
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static int minvalue = int.MinValue;
+        static int maxvalue = int.MaxValue;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] elements = { minvalue, maxvalue, 7, 2, minvalue};
+
+            NumberAppearsInArray.x = minvalue;
+            NumberAppearsInArray.NumberApperasinArray(elements);
+        }
+        public void testMeth2()
+        {
+            Assert.AreEqual(8, NumberAppearsInArray.counter);
+            NumberAppearsInArray.counter =0;
+
+        }
+    }
+}
