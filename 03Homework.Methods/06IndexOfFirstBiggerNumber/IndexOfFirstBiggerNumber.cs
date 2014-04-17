b4d73099321d62639be12a5e0c61461b4e3b339a
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 14:56:14 +0200

    methods

---
+++ 03Homework.Methods/06IndexOfFirstBiggerNumber/IndexOfFirstBiggerNumber.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+class FirstBiggerThenNeighbors
+{
+    static int BiggerNeighborFinder(int[] array)
+    {
+        int index = 0;
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if ((array[i] > array[i + 1]) && (array[i] > array[i - 1]))
+            {
+                index = i;
+                break;
+            }
+            else
+            {
+                index = -1;
+            }
+        }
+        return index;
+
+    }
+    static void Main()
+    {
+        int[] myArray = { 1, 2, 3, 4, 5, 4, 7, 8 };
+        Console.WriteLine("the number with index {0}, is the first bigger than its neighbors.", BiggerNeighborFinder(myArray));
+    }
+}
+
