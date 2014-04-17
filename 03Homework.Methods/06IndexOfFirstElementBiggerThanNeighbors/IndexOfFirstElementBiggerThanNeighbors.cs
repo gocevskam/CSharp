54163d8c6cd929cdb4546ad0a6997024503f44b1
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 14:56:29 +0200

    m

---
+++ 03Homework.Methods/06IndexOfFirstElementBiggerThanNeighbors/IndexOfFirstElementBiggerThanNeighbors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BiggerThanNeighbors;
+
+    class IndexOfFirstElementBiggerThanNeighbors
+    {
+      static void int FindsBigerNumber()
+      {
+
+      }
+        static void Main()
+        {
+            int[] array = { 2, 3, 4, 5, 6, 9, 5, 4, 10, 8 };
+        }
+    }
+
---
+++ 03Homework.Methods/07ReversedDecimal/ReversedDecimal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class ReversedDecimal
+{
+    static char[] ReverseDigits(decimal dec)
+    {
+        string stringDigits = Convert.ToString(dec);
+        char[] reversedNumber = new char[stringDigits.Length];
+        for (int i = 0, j = stringDigits.Length - 1; i < stringDigits.Length; j--, i++)
+        {
+            reversedNumber[j] = stringDigits[i];
+        }
+        return reversedNumber;
+    }
+    static void PrintArray(char[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i]);
+        }
+        Console.WriteLine();
+    }
+ 
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a decimal number:");
+        decimal number = decimal.Parse(Console.ReadLine());
+        string reversed = Convert.ToString(ReverseDigits(number));
+        PrintArray(ReverseDigits(number));
+    }
+}
