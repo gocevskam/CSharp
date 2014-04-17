f9dfb97bb4d62f7274c21485f5ae72810700c0f4
gocevskam <gocevska88@yahoo.co.uk>
2014-04-17 15:43:36 +0200

    Using Classes and Objects

---
+++ 05.HomeworkUseingClasses/06StringToInt/StringToInt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+    class StringToInt
+    {
+        static void Main()
+        {
+            string myString = "34 26 55 55 34";
+            int sum = SumOfStrings(myString);
+
+            Console.WriteLine(sum);
+        }
+            private static int SumOfStrings(string myString)
+        {
+            int sum;
+            char[] charSeparators = new char[] { ' ' };
+            string[] splittedArray = myString.Split(charSeparators,StringSplitOptions.RemoveEmptyEntries);
+ 
+            sum = CompleteSum(splittedArray);
+ 
+            return sum;
+        }
+ 
+        private static int CompleteSum(string[] splittedArray)
+        {
+            int total = 0;
+            foreach (string number in splittedArray)
+            {
+                total += int.Parse(number);
+            }
+ 
+            return total;
+        }
+        }
+    
